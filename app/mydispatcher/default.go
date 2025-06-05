7cf7d4b67a00d9cbf44b27292f255edeed449f2c
Aqr-K <95741669+Aqr-K@users.noreply.github.com>
2025-06-06 02:20:38 +0800

    fix: DNS

--- app/mydispatcher/default.go
+++ app/mydispatcher/default.go
@@ -385,11 +385,17 @@ func sniffer(ctx context.Context, cReader *cachedReader, metadataOnly bool, netw
 func (d *DefaultDispatcher) routedDispatch(ctx context.Context, link *transport.Link, destination net.Destination) {
 	outbounds := session.OutboundsFromContext(ctx)
 	ob := outbounds[len(outbounds)-1]
-	if hosts, ok := d.dns.(dns.HostsLookup); ok && destination.Address.Family().IsDomain() {
-		proxied := hosts.LookupHosts(ob.Target.String())
-		if proxied != nil {
+	// START of MODIFICATION
+	if d.dns != nil && destination.Address.Family().IsDomain() {
+		ips, _, err := d.dns.LookupIP(destination.Address.Domain(), dns.IPOption{
+			IPv4Enable: true,
+			IPv6Enable: true,
+		})
+		if err == nil && len(ips) > 0 {
+			// 成功解析到 IP，选择第一个 IP 作为新的目标地址
+			proxied := net.IPAddress(ips[0])
 			ro := ob.RouteTarget == destination
-			destination.Address = *proxied
+			destination.Address = proxied
 			if ro {
 				ob.RouteTarget = destination
 			} else {
